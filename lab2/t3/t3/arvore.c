0b104e9c2ce9c3b2a54a63777d9e5319bc35eb3f
Tales Cruz da Silva <85706724+Talescruzs@users.noreply.github.com>
2024-06-06 11:25:16 -0300

    iniciando alguma coisa

--- lab2/t3/t3/arvore.c
+++ lab2/t3/t3/arvore.c
@@ -1,17 +1,55 @@
-#include "telag.h"
+//#include "telag.h"
 #include "arvore.h"
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct arv{
-    char * palavra;
-    int x;
-    int y;
-    int alt;
-    struct arv *dir;
-    struct arv *esq;
-} *Arv;
+Arv cria_arv(char *palavra){
+    Arv arv = malloc(sizeof(struct arv));
+    arv->palavra = palavra;
+    arv->alt = 0;
+    arv->largura = strlen(palavra);
+    arv->dir = NULL;
+    arv->esq = NULL;
+    arv->x = 0;
+    arv->y = 0;
+    return arv;
+}
 
-Arv cria_arv(void){
-    
+void printa_arv(Arv base){
+    if(base == NULL){
+        printf("\n");
+        return;
+    }
+    if(base->dir == NULL){ 
+        printf("%s\n", base->palavra);
+        return;
+    }
+    printf("    ");
+    printa_arv(base->dir);
+    printf("%s\n", base->palavra);
+    printa_arv(base->esq);
+}
+
+Arv insere_arv(Arv base, Arv novo){
+    if(base == NULL) return novo;
+    if(strcmp(base->palavra, novo->palavra)>0){
+        base = insere_arv(base->esq, novo);
+    }
+    else if(strcmp(base->palavra, novo->palavra)<0){
+        base = insere_arv(base->dir, novo);
+    }
+    return base;
+}
+
+Arv remove_arv(Arv base, char *palavra){
+    if(base == NULL) return base;
+    if(strcmp(base->palavra, palavra)>0){
+        remove_arv(base->esq, palavra);
+    }
+    else if(strcmp(base->palavra, palavra)<0){
+        remove_arv(base->dir, palavra);
+    }
+    return base;
 }
