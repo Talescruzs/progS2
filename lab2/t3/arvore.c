13b5353354ead1c967245d63da64da908cda8c60
Talescruzs <tcs2017@hotmail.com>
2024-06-10 21:40:50 -0300

    commitando ultimas coisas

--- lab2/t3/arvore.c
+++ lab2/t3/arvore.c
@@ -18,20 +18,16 @@ Arv cria_arv(char *palavra){
 void printa_arv(Arv base, int espaco) {
     if (base == NULL) return;
 
-    // Aumenta a distância entre níveis
     espaco += 10;
 
-    // Imprime a subárvore direita
     printa_arv(base->dir, espaco);
 
-    // Imprime o nó atual após o espaço adequado
     printf("\n");
     for (int i = 10; i < espaco; i++) {
         printf(" ");
     }
     printf("%s\n", base->palavra);
 
-    // Imprime a subárvore esquerda
     printa_arv(base->esq, espaco);
 }
 
@@ -51,13 +47,46 @@ Arv insere_arv(Arv base, char *palavra){
     return base;
 }
 
-Arv remove_arv(Arv base, char *palavra){
-    if(base == NULL) return base;
-    if(strcmp(base->palavra, palavra)>0){
-        remove_arv(base->esq, palavra);
+Arv insere_galho(Arv base, Arv galho){
+    printf("VERIFICA\n");
+    if(base == NULL) return galho;
+    printf("PASSOU1\n");
+    if(strcmp(base->palavra, galho->palavra)>0){
+        printf("ESQUERDA\n");
+        base->esq = insere_arv(base->esq, galho->palavra);
     }
-    else if(strcmp(base->palavra, palavra)<0){
-        remove_arv(base->dir, palavra);
+    else if(strcmp(base->palavra, galho->palavra)<0){
+        printf("DIREITA\n");
+        base->dir = insere_arv(base->dir, palavra);
+    }
+    printf("PASSOU2\n");
+    return base;
+}
+
+Arv remove_arv(Arv base, char *palavra) {
+    if (base == NULL) return NULL;
+
+    if (strcmp(palavra, base->palavra) < 0) {
+        base->esq = remove_arv(base->esq, palavra);
+    } else if (strcmp(palavra, base->palavra) > 0) {
+        base->dir = remove_arv(base->dir, palavra);
+    } else {
+        if (base->esq == NULL) {
+            Arv temp = base->dir;
+            free(base);
+            return temp;
+        } else if (base->dir == NULL) {
+            Arv temp = base->esq;
+            free(base->palavra);
+            free(base);
+            return temp;
+        }
+
+        // Nó com dois filhos
+        Arv temp = min_valor_no(base->dir);
+        free(base->palavra);
+        base->palavra = strdup(temp->palavra);
+        base->dir = remove_arv(base->dir, temp->palavra);
     }
     return base;
 }
