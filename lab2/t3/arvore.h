7affb8c0f67f7b3b8cc2590476da2762f8f2067d
Tales Cruz da Silva <85706724+Talescruzs@users.noreply.github.com>
2024-06-11 11:56:05 -0300

    mudei tudo na aula

--- lab2/t3/arvore.h
+++ lab2/t3/arvore.h
@@ -1,18 +1,27 @@
 #ifndef _arvore_h_ 
 #define _arvore_h_
-
-typedef struct arv{
-    struct arv *dir;
-    struct arv *esq;
+typedef struct data{
     char *palavra;
-    int alt;
     int x;
     int y;
+} *dado_t;
+typedef struct _no no_t;
+struct _no {
+    dado_t val;
     int largura;
-} *Arv;
-Arv cria_arv(char *palavra);
-Arv insere_arv(Arv base, char *palavra);
-Arv remove_arv(Arv base, char *palavra);
-void printa_arv(Arv base, int espaco);
+    int alt;
+    no_t *esq;
+    no_t *dir;
+};
+typedef no_t arv_t;
+
+int num_nos(arv_t *a);
+int altura(arv_t *a);
+arv_t *no(char *palavra, arv_t *esq, arv_t *dir);
+arv_t *cria_no_vazio(void);
+void insere(no_t *a, char *palavra);
+void remove_no(arv_t *a, char *p);
+void printa_arv(arv_t *base, int espaco);
+dado_t busca(arv_t *a, char *palavra);
 
 #endif
