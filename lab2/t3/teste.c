f00bb521b8e29636ced057a2302b6c99bc2b055e
Talescruzs <tcs2017@hotmail.com>
2024-06-18 22:03:51 -0300

    add ultimas coisas do dia

--- lab2/t3/teste.c
+++ lab2/t3/teste.c
@@ -1,4 +1,5 @@
 #include "arvore.h"
+#include "tecla.h"
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,17 +14,34 @@ void waitFor (unsigned int secs) {
 int main(){
     Arv *a = cria_arv();
     char proxima_palavra [10];
+    char palavra_digitada [10];
+    char letra_digitada;
     int i = 0;
     int tempo_aleatorio;
+    int clock;
+    tecla_ini();
     while(i<100){
         cria_palavra(proxima_palavra);
         printf("proxima palavra :%s\n\n", proxima_palavra);
         printa_arv(a, 0);
         tempo_aleatorio = rand()%5;
-        waitFor(tempo_aleatorio);
+        clock = time(0);
+        while(time(0) < clock+tempo_aleatorio){
+            letra_digitada = tecla_le_char();
+            if(letra_digitada=='\n'){
+                a = remover_no(a, palavra_digitada);
+                palavra_digitada = '\0';
+            }
+            else{
+                strcat(palavra_digitada, &letra_digitada);
+                printf("%s\n", palavra_digitada);
+            }
+        }
+        // waitFor(tempo_aleatorio);
         system("clear");
         a = insere(a, proxima_palavra);
         i++;
     }
+    tecla_fim();
     return 0;
 }
