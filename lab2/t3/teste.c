ecbc10d5549b455eeaf5dcbd294da8503f4ec284
Talescruzs <tcs2017@hotmail.com>
2024-06-18 21:44:57 -0300

    add tecla do prof e tempo aleatorio pra add palavra

--- lab2/t3/teste.c
+++ lab2/t3/teste.c
@@ -3,18 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+void waitFor (unsigned int secs) {
+    unsigned int retTime = time(0) + secs;   // Get finishing time.
+    while (time(0) < retTime);               // Loop until it arrives.
+}
 
 int main(){
     Arv *a = cria_arv();
-    char teste [10];
+    char proxima_palavra [10];
     int i = 0;
-    while(i<10){
-        cria_palavra(teste);
-        // printf("%s\n", teste);
-        a = insere(a, teste);
-        // printa_arv(a, 0);
+    int tempo_aleatorio;
+    while(i<100){
+        cria_palavra(proxima_palavra);
+        printf("proxima palavra :%s\n\n", proxima_palavra);
+        printa_arv(a, 0);
+        tempo_aleatorio = rand()%5;
+        waitFor(tempo_aleatorio);
+        system("clear");
+        a = insere(a, proxima_palavra);
         i++;
     }
-    printa_arv(a, 0);
     return 0;
 }
